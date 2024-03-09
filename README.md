2a0d555c6cf682c5c9b5e406415f12a26abfacbe
Neto Santos <netosantosbatista@outlook.com>
2024-03-09 20:07:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# SimpleAutoMapper
+
+SimpleAutoMapper is a lightweight and easy-to-use library for automatic object mapping in C#. It allows you to map an object to another one efficiently and intuitively.
+
+## How to install
+
+SimpleAutoMapper is available as a NuGet package. You can install it using the NuGet package manager in Visual Studio or through the command line.
+
+```shell
+Install-Package SimpleAutoMapper -Version 1.0.0
+```
+You can also download the package directly from the NuGet website at https://www.nuget.org/packages/SimpleAutoMapper/1.0.0.
+
+How to use
+To use SimpleAutoMapper, first add the following line at the top of your file:
+
+```C#
+using SimpleAutoMapper;
+var response = AutoMapper.Map<ResponseModel>(request);
+```
+
+In this example, request is the object that you want to map and ResponseModel is the type of the target object. The Map<T> method creates a new instance of ResponseModel, maps the properties of the request object to the ResponseModel object and returns the ResponseModel object.
+
+## Features
+* Automatic mapping of properties with the same name and type.
+* Support for primitive types, strings, Guids, DateTimes and nullable types.
+* Recursive mapping for complex objects.
+* Support for lists and collections.
+
+## Limitations
+* The properties must have public setters to be mapped.
+* The mapping is not done in depth for lists or collections of complex objects. Each object in the list or collection is mapped individually.
+
+We hope you find SimpleAutoMapper useful for your C# projects! If you have any questions or suggestions, feel free to open an issue.
---
+++ SimpleAutoMapper/AutoMapper.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+
+namespace SimpleAutoMapper
+{
+    public class AutoMapper
+    {
+        public static T? Map<T>(object source) where T : new()
+        {
+            return (T?)Map(source, typeof(T));
+        }
+
+        public static object? Map(object source, Type targetType)
+        {
+            var target = Activator.CreateInstance(targetType);
+            var sourceType = source.GetType();
+            var sourceProperties = sourceType.GetProperties();
+            var targetProperties = targetType.GetProperties();
+
+            foreach (var sourceProperty in sourceProperties)
+            {
+                foreach (var targetProperty in targetProperties)
+                {
+                    if (sourceProperty.Name != targetProperty.Name)
+                    {
+                        break;
+                    }
+
+                    if (sourceProperty.PropertyType.IsGenericType &&
+                        sourceProperty.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
+                    {
+                        var itemType = targetProperty.PropertyType.GetGenericArguments()[0];
+                        var sourceList = (IList?)sourceProperty.GetValue(source);
+                        var targetList = (IList?)Activator.CreateInstance(typeof(List<>).MakeGenericType(itemType));
+
+                        if (sourceList == null || targetList == null)
+                        {
+                            break;
+                        }
+
+                        foreach (var item in sourceList)
+                        {
+                            targetList.Add(Map(item, itemType));
+                        }
+
+                        targetProperty.SetValue(target, targetList);
+                        break;
+                    }
+
+                    if (sourceProperty.PropertyType.IsPrimitive ||
+                        sourceProperty.PropertyType.IsEnum ||
+                        sourceProperty.PropertyType.Equals(typeof(string)) ||
+                        sourceProperty.PropertyType.IsArray ||
+                        sourceProperty.PropertyType.Equals(typeof(Guid)) ||
+                        sourceProperty.PropertyType.Equals(typeof(DateTime)) ||
+                        Nullable.GetUnderlyingType(sourceProperty.PropertyType) != null
+                       )
+                    {
+                        if (sourceProperty.PropertyType != targetProperty.PropertyType || !targetProperty.CanWrite)
+                        {
+                            break;
+                        }
+                        targetProperty.SetValue(target, sourceProperty.GetValue(source));
+                    }
+                    else
+                    {
+                        var sourceValue = sourceProperty.GetValue(source);
+                        if (sourceValue == null)
+                        {
+                            break;
+                        }
+
+                        var targetValue = Map(sourceValue, targetProperty.PropertyType);
+                        if (targetValue == null)
+                        {
+                            break;
+                        }
+                        targetProperty.SetValue(target, targetValue);
+                    }
+                    break;
+                }
+            }
+            return target;
+        }
+
+    }
+}
